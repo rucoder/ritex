382f28c7eef6fde83c97f442db4c9e116282c70e
Michael Malyshev <miha.malyshev@yandex.ru>
2013-05-06 19:12:58 +0400

    Add error checking for SQL API calls

--- src/DataLoggerThread.cpp
+++ src/DataLoggerThread.cpp
@@ -19,15 +19,34 @@ DataLoggerThread::DataLoggerThread(std::string db_name)
 
 bool DataLoggerThread::Insert(DBDataPacket* event)
 {
-	syslog(LOG_ERR, "[SQL] Inserting data...");
+	syslog(LOG_ERR, "[SQL] Inserting data: chId=%d, paramId=%d, td=%lu %s val=%g",event->getChannelId(), event->getParamId(),
+	 event->getRegisterDate(), Utils::TimeToString(event->getRegisterDate()).c_str(), event->getValue());
 	//bind parameters first
-	sqlite3_bind_int(m_pStm, 1, event->getChannelId());
-	sqlite3_bind_int(m_pStm, 2, event->getParamId());
-	sqlite3_bind_text(m_pStm, 3, Utils::TimeToString(event->getRegisterDate()).c_str(), -1, SQLITE_TRANSIENT);
-	sqlite3_bind_double(m_pStm, 4, event->getValue());
+	if(sqlite3_bind_int(m_pStm, 1, event->getChannelId()) != SQLITE_OK) {
+		syslog(LOG_ERR, "[SQL] Failed to bind int 1");
+		return false;
+	}
+		
+	if(sqlite3_bind_int(m_pStm, 2, event->getParamId())!= SQLITE_OK) {
+		syslog(LOG_ERR, "[SQL] Failed to bind int 2");
+		return false;
+	}
+	if(sqlite3_bind_text(m_pStm, 3, Utils::TimeToString(event->getRegisterDate()).c_str(), -1, SQLITE_TRANSIENT)!= SQLITE_OK) {
+		syslog(LOG_ERR, "[SQL] Failed to bind text");
+		return false;
+	}
+	if(sqlite3_bind_double(m_pStm, 4, event->getValue())!= SQLITE_OK) {
+		syslog(LOG_ERR, "[SQL] Failed to bind double");
+		return false;
+	}
 
-	sqlite3_step(m_pStm);
-	return sqlite3_reset(m_pStm) == SQLITE_OK;
+	if (sqlite3_step(m_pStm) != SQLITE_DONE) {
+		syslog(LOG_ERR, "[SQL] Failed to step");
+		return false;
+	}
+	sqlite3_reset(m_pStm);
+	sqlite3_clear_bindings(m_pStm);
+	return true;
 }
 
 
